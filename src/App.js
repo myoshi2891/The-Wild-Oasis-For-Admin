80706e17da0f037255d474e8b66ddb54866bb51d
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-07-07 13:01:17 +0900

    Creating useLocalStorageState

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useEffect, useRef, useState } from "react";
 import StarRating from "./StarRating";
+import { useLocalStorageState } from "./useLocalStorageState";
 import { useMovies } from "./useMovies";
 
 const average = (arr) =>
@@ -11,11 +12,7 @@ export default function App() {
 	const [selectedId, setSelectedId] = useState(null);
 	const { movies, isLoading, error } = useMovies(query);
 
-	// const [watched, setWatched] = useState([]);
-	const [watched, setWatched] = useState(function () {
-		const storedValue = localStorage.getItem("watched");
-		return JSON.parse(storedValue);
-	});
+	const [watched, setWatched] = useLocalStorageState([], "watched");
 
 	function handleSelectMovie(id) {
 		setSelectedId((selectedId) => (id === selectedId ? null : id));
@@ -27,20 +24,11 @@ export default function App() {
 
 	function handleAddWatched(movie) {
 		setWatched((watched) => [...watched, movie]);
-
-		// localStorage.setItem("watched", JSON.stringify([...watched, movie]));
 	}
 
 	function handleDeleteWatched(id) {
 		setWatched((watched) => watched.filter((movie) => movie.imdbID !== id));
 	}
-
-	useEffect(
-		function () {
-			localStorage.setItem("watched", JSON.stringify(watched));
-		},
-		[watched]
-	);
 
 	return (
 		<>
