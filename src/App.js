93739eb6706ca57a685fa99e28fabf7ebce87fa8
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-07-07 12:03:03 +0900

    Refs to Select DOM Elements

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import StarRating from "./StarRating";
 
 const average = (arr) =>
@@ -166,12 +166,30 @@ function Logo() {
 }
 
 function Search({ query, setQuery }) {
-	useEffect(function () {
-		const el = document.querySelector(".search");
-		console.log(el);
-		el.focus();
-	}, []);
-	
+	const inputEl = useRef(null);
+
+	useEffect(
+		function () {
+			function callback(e) {
+				if (document.activeElement === inputEl.current) return;
+				if (e.code === "Enter") {
+					inputEl.current.focus();
+					setQuery("");
+				}
+			}
+			document.addEventListener("keydown", callback);
+
+			return () => document.addEventListener("keydown", callback);
+		},
+		[setQuery]
+	);
+
+	// useEffect(function () {
+	// 	const el = document.querySelector(".search");
+	// 	console.log(el);
+	// 	el.focus();
+	// }, []);
+
 	return (
 		<input
 			className="search"
@@ -179,6 +197,7 @@ function Search({ query, setQuery }) {
 			placeholder="Search movies..."
 			value={query}
 			onChange={(e) => setQuery(e.target.value)}
+			ref={inputEl}
 		/>
 	);
 }
@@ -367,7 +386,7 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
 						</div>
 					</header>
 
-				{/* <p>{avgRating}</p> */}
+					{/* <p>{avgRating}</p> */}
 					<section>
 						<div className="rating">
 							{!isWatched ? (
