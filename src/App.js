7918f79ba414fc4338397fb44dc4b5ec60387f48
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-07-08 20:44:03 +0900

    Loading Questions from a Fake API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
 
+const initialState = {
+	questions: [],
+	status: "loading",
+};
+
+function reducer(state, action) {
+	switch (action.type) {
+		case "dataRecieved":
+			return {
+				...state,
+				questions: action.payload,
+				status: "ready",
+			};
+		case "dataFailed":
+			return {
+				...state,
+				status: "error",
+			};
+		default:
+			throw new Error("Action Unknown...");
+	}
+}
+
 export default function App() {
+	const [state, dispatch] = useReducer(reducer, initialState);
+
+	useEffect(function () {
+		fetch("http://localhost:8000/questions")
+			.then((res) => res.json())
+			.then((data) => dispatch({ type: "dataRecieved", payload: data }))
+			.catch((err) => dispatch({ type: "dataFailed" }));
+
+		return () => {};
+	}, []);
+
 	return (
 		<div className="app">
 			<Header />
