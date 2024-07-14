f0a177b2c24cbfcc8119598eed4e4d5014f346fd
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-07-14 13:55:19 +0900

    Adding Fake Authentication: Implementing Login

--- src/contexts/FakeAuthContext.jsx
+++ src/contexts/FakeAuthContext.jsx
@@ -21,16 +21,16 @@ const FAKE_USER = {
 	name: "Jack",
 	email: "jack@example.com",
 	password: "qwerty",
-	avatar: "https://i.pravatar.cc/100?u=zz",
+	avatar: "https://i.pravatar.cc/400?u=aa",
 };
 
 function AuthProvider({ children }) {
-	connst[({ user, isAuthenticated }, dispatch)] = useReducer(
+	const [{ user, isAuthenticated }, dispatch] = useReducer(
 		reducer,
 		initialState
 	);
 	function login(email, password) {
-		if (email === FAKE_USER.email && password === FAKE_PASSWORD.password)
+		if (email === FAKE_USER.email && password === FAKE_USER.password)
 			dispatch({ type: "login", payload: FAKE_USER });
 	}
 
@@ -56,6 +56,7 @@ function useAuth() {
 	const context = useContext(AuthContext);
 	if (context === undefined)
 		throw new Error("AuthContext was used outside AuthProvider...");
+	return context;
 }
 
 export { AuthProvider, useAuth };
