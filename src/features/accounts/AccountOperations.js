b353c5112ea3fdb4ebe0172c8e88b5df98b8c88e
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-07-17 23:09:52 +0900

    Creating the Account Slice

--- src/features/accounts/AccountOperations.js
+++ src/features/accounts/AccountOperations.js
@@ -17,11 +17,10 @@ function AccountOperations() {
 		isLoading,
 	} = useSelector((store) => store.account);
 
-	console.log(balance);
-
 	function handleDeposit() {
 		if (!depositAmount) return;
-		dispatch(deposit(depositAmount, currency));
+		// dispatch(deposit(depositAmount, currency));
+		dispatch(deposit(depositAmount));
 		setDepositAmount("");
 		setCurrency("USD");
 	}
