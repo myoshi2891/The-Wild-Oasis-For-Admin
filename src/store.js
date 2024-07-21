83956f6fa2987241f65ce149e40bf7a2aa85a5eb
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-07-21 15:03:33 +0900

    Modeling the User State With Redux Toolkit

---
+++ src/store.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+import { configureStore } from "@reduxjs/toolkit";
+import userReducer from "./features/user/userSlice";
+
+const store = configureStore({
+  reducer: {
+    user: userReducer,
+  },
+});
+
+export default store;
