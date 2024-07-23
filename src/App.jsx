b0f32336b6ac3d7d211021eafb8f6d2d2e73aa06
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-07-23 21:34:46 +0900

    Introduction to Styled Components

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,39 @@ const H1 = styled.h1`
 	font-weight: 600;
 `;
 
+const Button = styled.button`
+	font-size: 1.4rem;
+	padding: 1.2rem 1.6rem;
+	font-weight: 500;
+	border: none;
+	border-radius: 7px;
+	background-color: purple;
+	color: white;
+	cursor: pointer;
+
+	margin: 20px;
+`;
+
+const Input = styled.input`
+	border: 1px solid #ddd;
+	border-radius: 5px;
+	padding: 0.8rem 1.2rem;
+`;
+
+const StyledApp = styled.main`
+	background-color: orangered;
+	padding: 20px;
+`;
+
 function App() {
 	return (
-		<div>
+		<StyledApp>
 			<H1>The Wild Oasis</H1>
-		</div>
+			<Button onClick={() => alert("Check in")}>Check in</Button>
+			<Button onClick={() => alert("Check out")}>Check out</Button>
+
+			<Input type="number" placeholder="Number of guest" />
+		</StyledApp>
 	);
 }
 
