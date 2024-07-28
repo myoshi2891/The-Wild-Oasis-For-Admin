9c829b9ef4820dd4b62628d2a5dca43ada167a9d
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-07-28 15:27:31 +0900

    Editing a Cabin

--- src/features/cabins/CreateCabinForm copy.jsx
+++ src/features/cabins/CreateCabinForm copy.jsx
@@ -1,29 +1,59 @@
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import toast from "react-hot-toast";
+import styled from "styled-components";
 
 import Input from "../../ui/Input";
 import Form from "../../ui/Form";
 import Button from "../../ui/Button";
 import FileInput from "../../ui/FileInput";
 import Textarea from "../../ui/Textarea";
+import { useForm } from "react-hook-form";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+import { createCabin } from "../../services/apiCabins";
 import FormRow from "../../ui/FormRow";
 
-import { useForm } from "react-hook-form";
-import { createEditCabin } from "../../services/apiCabins";
+// const FormRow = styled.div`
+// 	display: grid;
+// 	align-items: center;
+// 	grid-template-columns: 24rem 1fr 1.2fr;
+// 	gap: 2.4rem;
 
-function CreateCabinForm({ cabinToEdit = {} }) {
-	const { id: editId, ...editValues } = cabinToEdit;
-	const isEditSession = Boolean(editId);
+// 	padding: 1.2rem 0;
 
-	const { register, handleSubmit, reset, getValues, formState } = useForm({
-		defaultValues: isEditSession ? editValues : {},
-	});
+// 	&:first-child {
+// 		padding-top: 0;
+// 	}
+
+// 	&:last-child {
+// 		padding-bottom: 0;
+// 	}
+
+// 	&:not(:last-child) {
+// 		border-bottom: 1px solid var(--color-grey-100);
+// 	}
+
+// 	&:has(button) {
+// 		display: flex;
+// 		justify-content: flex-end;
+// 		gap: 1.2rem;
+// 	}
+// `;
+
+// const Label = styled.label`
+// 	font-weight: 500;
+// `;
+
+// const Error = styled.span`
+// 	font-size: 1.4rem;
+// 	color: var(--color-red-700);
+// `;
+
+function CreateCabinForm() {
+	const { register, handleSubmit, reset, getValues, formState } = useForm();
 	const { errors } = formState;
 
 	const queryClient = useQueryClient();
-
-	const { mutate: createCabin, isLoading: isCreating } = useMutation({
-		mutationFn: createEditCabin,
+	const { mutate, isLoading: isCreating } = useMutation({
+		mutationFn: createCabin,
 		onSuccess: () => {
 			toast.success("New cabin successfully created.");
 			queryClient.invalidateQueries({ queryKey: ["cabins"] });
@@ -32,25 +62,8 @@ function CreateCabinForm({ cabinToEdit = {} }) {
 		onError: (err) => toast.error(err.message),
 	});
 
-	const { mutate: editCabin, isLoading: isEditing } = useMutation({
-		mutationFn: ({ newCabinData, id }) => createEditCabin(newCabinData, id),
-		onSuccess: () => {
-			toast.success("Cabin successfully edited.");
-			queryClient.invalidateQueries({ queryKey: ["cabins"] });
-			reset();
-		},
-		onError: (err) => toast.error(err.message),
-	});
-
-	const isWorking = isCreating || isEditing;
-
 	function onSubmit(data) {
-		const image =
-			typeof data.image === "string" ? data.image : data.image[0];
-
-		if (isEditSession)
-			editCabin({ newCabinData: { ...data, image }, id: editId });
-		else createCabin({ ...data, image: image });
+		mutate({ ...data, image: data.image[0] });
 	}
 
 	function onError(errors) {
@@ -63,7 +76,7 @@ function CreateCabinForm({ cabinToEdit = {} }) {
 				<Input
 					type="text"
 					id="name"
-					disabled={isWorking}
+					disabled={isCreating}
 					{...register("name", {
 						required: "This field is required",
 					})}
@@ -77,7 +90,7 @@ function CreateCabinForm({ cabinToEdit = {} }) {
 				<Input
 					type="number"
 					id="maxCapacity"
-					disabled={isWorking}
+					disabled={isCreating}
 					{...register("maxCapacity", {
 						required: "This field is required",
 						min: {
@@ -95,7 +108,7 @@ function CreateCabinForm({ cabinToEdit = {} }) {
 				<Input
 					type="number"
 					id="regularPrice"
-					disabled={isWorking}
+					disabled={isCreating}
 					{...register("regularPrice", {
 						required: "This field is required",
 						min: {
@@ -110,7 +123,7 @@ function CreateCabinForm({ cabinToEdit = {} }) {
 				<Input
 					type="number"
 					id="discount"
-					disabled={isWorking}
+					disabled={isCreating}
 					defaultValue={0}
 					{...register("discount", {
 						required: "This field is required",
@@ -128,7 +141,7 @@ function CreateCabinForm({ cabinToEdit = {} }) {
 				<Textarea
 					type="number"
 					id="description"
-					disabled={isWorking}
+					disabled={isCreating}
 					defaultValue=""
 					{...register("description", {
 						required: "This field is required",
@@ -142,9 +155,7 @@ function CreateCabinForm({ cabinToEdit = {} }) {
 					accept="image/*"
 					type="file"
 					{...register("image", {
-						required: isEditSession
-							? false
-							: "This field is required",
+						required: "This field is required",
 					})}
 				/>
 			</FormRow>
@@ -154,9 +165,7 @@ function CreateCabinForm({ cabinToEdit = {} }) {
 				<Button variation="secondary" type="reset">
 					Cancel
 				</Button>
-				<Button disabled={isWorking}>
-					{isEditSession ? "Edit cabin" : "Create new cabin"}
-				</Button>
+				<Button disabled={isCreating}>Add cabin</Button>
 			</FormRow>
 		</Form>
 	);
--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -11,15 +11,29 @@ export async function getCabins() {
 	return data;
 }
 
-export async function createCabin(newCabin) {
+export async function createEditCabin(newCabin, id) {
+	console.log(newCabin, id);
+	const hasImagePath = newCabin.image?.startsWith?.(supabaseUrl);
 	const imageName = `${Math.random()}-${newCabin.image.name}`.replaceAll(
 		"/",
 		""
 	);
-	const imagePath = `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
-	const { data, error } = await supabase
-		.from("cabins")
-		.insert([{ ...newCabin, image: imagePath }]);
+
+	const imagePath = hasImagePath
+		? newCabin.image
+		: `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
+
+	let query = supabase.from("cabins");
+
+	if (!id) query = query.insert([{ ...newCabin, image: imagePath }]);
+
+	if (id)
+		query = query
+			.update({ ...newCabin, image: imagePath })
+			.eq("id", id)
+			.select();
+
+	const { data, error } = await query.select().single();
 
 	if (error) {
 		console.error(error);
