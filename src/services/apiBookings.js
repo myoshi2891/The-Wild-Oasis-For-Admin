7757f268a8c258948cc578629f74374e22c6e28e
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-08-03 16:58:42 +0900

    API-Side Sorting: Sorting Bookings

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -8,8 +8,13 @@ export async function getBookings({ filter, sortBy }) {
 			"id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName, email)"
 		);
 
-	if (filter !== null)
+	if (filter)
 		query = query[filter.method || "eq"](filter.field, filter.value);
+  
+  if (sortBy)
+		query = query.order(sortBy.field, {
+			ascending: sortBy.direction === "asc",
+		});
 
 	const { data, error } = await query;
 
