5e0a7860eba8349642f53f4636408e78baf900b1
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-08-03 14:30:48 +0900

    Client-Side Sorting: Sorting Cabins

--- src/ui/Filter.jsx
+++ src/ui/Filter.jsx
@@ -50,7 +50,8 @@ function Filter({ filterField, options }) {
 				<FilterButton
 					key={option.value}
 					onClick={() => handleClick(option.value)}
-					active={(option.value === currentFilter).toString()}
+					active={option.value === currentFilter ? "true" : ""}
+					disabled={option.value === currentFilter}
 				>
 					{option.label}
 				</FilterButton>
