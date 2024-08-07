1e5b3a751bc5f077b702221e26ff3e12a3150003
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-08-07 22:39:23 +0900

    Implementing Dark Mode With CSS Variables

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -57,11 +57,13 @@ export async function updateCurrentUser({ password, fullName, avatar }) {
 
 	if (storageError) throw new Error(storageError.message);
 
-	const { data: updatedUser, error: error2 } = supabase.auth.updateUser({
-		data: {
-			avatar: `${supabaseUrl}/storage/v1/object/public/avatars/${fileName}`,
-		},
-	});
+	const { data: updatedUser, error: error2 } = await supabase.auth.updateUser(
+		{
+			data: {
+				avatar: `${supabaseUrl}/storage/v1/object/public/avatars/${fileName}`,
+			},
+		}
+	);
 	if (error2) throw new Error(error2.message);
 	return updatedUser;
 }
